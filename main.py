27868031b7860d6a50b5e1ea08a552029f004cc1
unknown <prashantmit57@gmail.com>
2023-01-31 00:18:08 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,106 @@
+import random
+import array
+from tkinter import *
+from tkinter import messagebox
+
+
+# PASSWORD GENERATOR
+def password_generator():
+    MAX_LEN = 12
+    DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                         'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                         'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                         'z']
+
+    UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                         'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                         'Z']
+
+    SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+               '*', '(', ')', '<']
+
+    # combines all the character arrays above to form one array
+    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+
+    # randomly select at least one character from each character set above
+    rand_digit = random.choice(DIGITS)
+    rand_upper = random.choice(UPCASE_CHARACTERS)
+    rand_lower = random.choice(LOCASE_CHARACTERS)
+    rand_symbol = random.choice(SYMBOLS)
+
+    temp_pass = rand_digit + rand_upper + rand_lower + rand_symbol
+
+    for x in range(MAX_LEN - 4):
+        temp_pass = temp_pass + random.choice(COMBINED_LIST)
+        temp_pass_list = array.array('u', temp_pass)
+        random.shuffle(temp_pass_list)
+
+    password = ""
+    for x in temp_pass_list:
+        password = password + x
+
+    password_entry.insert(0, password)
+
+
+# SAVE PASSWORD
+def save_password():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Error404", message="Please make sure to populate fields")
+    else:
+        ask_user = messagebox.askokcancel(title=website, message=f"Entered details are: \nEmail: {email} "
+                                          f"\nPassword: {password} \nIs it ok to save?")
+
+        if ask_user:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
+
+# UI SETUP
+
+window = Tk()
+window.title("Password Manager")
+window.config(pady=50, padx=50)
+
+canvas = Canvas(width=200, height=200)
+lock_image = PhotoImage(file="lock.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_entry = Entry(width=50)
+website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
+
+email_entry = Entry(width=50)
+email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "dummy@gmail.com")
+
+password_entry = Entry(width=32)
+password_entry.grid(column=1, row=3)
+
+# Button
+generate_pass_button = Button(text="Generate Password", width=14, command=password_generator)
+generate_pass_button.grid(column=2, row=3)
+
+add_button = Button(text="Add", width=42, command=save_password)
+add_button.grid(column=1, row=4, columnspan=2)
+
+window.mainloop()
